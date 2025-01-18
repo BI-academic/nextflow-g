7a386fad179c6223cb4f173abade60d7bc4c17e9
lioseo <superliomach3@gmail.com>
2025-01-18 10:30:26 +0900

    add uploads

--- src/views/pipelines.py
+++ src/views/pipelines.py
@@ -1,11 +1,41 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+from config import PipelinesConfig
+import os
 
 pipelines = Blueprint("pipelines", __name__)
+
+ALLOWED_EXTENSIONS = PipelinesConfig.ALLOWED_EXTENSIONS
+UPLOAD_FOLDER = PipelinesConfig.UPLOAD_FOLDER
 
 @pipelines.route("/")
 def pipelines_list():
     return render_template("pipelines.html")
 
-@pipelines.route("/install")
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@pipelines.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(UPLOAD_FOLDER, name)
+
+@pipelines.route('/install', methods=['GET', 'POST'])
 def pipeline_install():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(PipelinesConfig.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('unzip_local', name=filename))
+
     return render_template("pipeline_install.html")
